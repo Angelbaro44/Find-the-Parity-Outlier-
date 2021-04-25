13d42674687fdbccc86344f89e009fbb79be3b17
Angel Baro <angelbaro44@gmail.com>
2021-04-25 18:49:36 -0400

    Alt Solution added

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ function findOutlier(integers) {
 
 
 
+//Alt much more simple Solution
 
+// function findOutlier(int){
+//       var even = int.filter(a=>a%2==0);
+//       var odd = int.filter(a=>a%2!==0);
+//       return even.length==1? even[0] : odd[0];
+//     }
 
 
 
