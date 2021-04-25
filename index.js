de9716a5b351bbdd4cea213143bb32f13bee9673
Angel Baro <angelbaro44@gmail.com>
2021-04-25 18:47:51 -0400

    finished kata

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function findOutlier(integers){}
+
+function findOutlier(integers) {
+      if (integers.filter(x => x % 2).length == 1) {
+            return parseInt(integers.filter(x => x % 2).join())
+      }
+      if (integers.filter(x => { if (x % 2 == 0) { return x } }).length == 1) {
+            return parseInt(integers.filter(x => { if (x % 2 == 0) { return x } }).join())
+      }
+      else { return parseInt(integers.filter(x => !x % 2).join()) }
+}
+
 
 
 
@@ -17,11 +27,11 @@ console.log(findOutlier([0, 1, 2]))
 console.log(findOutlier([1, 2, 3]))
 // Expected Result: 2
 
-console.log(findOutlier([2,6,8,10,3]))
+console.log(findOutlier([2, 6, 8, 10, 3]))
 // Expected Result: 3
 
-console.log(findOutlier([0,0,3,0,0]))
+console.log(findOutlier([0, 0, 3, 0, 0]))
 // Expected Result: 3
 
-console.log(findOutlier([1,1,0,1,1]))
+console.log(findOutlier([1, 1, 0, 1, 1]))
 // Expected Result: 0
