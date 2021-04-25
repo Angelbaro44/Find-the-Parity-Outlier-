7195f8708260024f4e701e971d6289b3d83c807c
Angel Baro <angelbaro44@gmail.com>
2021-04-25 18:47:02 -0400

    Add test cases

---
+++ index.js
@@ -0,0 +1,27 @@
+function findOutlier(integers){}
+
+
+
+
+
+
+
+
+
+
+
+// Test Cases
+console.log(findOutlier([0, 1, 2]))
+// Expected Result: 1
+
+console.log(findOutlier([1, 2, 3]))
+// Expected Result: 2
+
+console.log(findOutlier([2,6,8,10,3]))
+// Expected Result: 3
+
+console.log(findOutlier([0,0,3,0,0]))
+// Expected Result: 3
+
+console.log(findOutlier([1,1,0,1,1]))
+// Expected Result: 0
